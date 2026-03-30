13c0abd3d170cffdc67d9fa5188f3bcc52f0d4f6
MauricioBarriosB <55992895+MauricioBarriosB@users.noreply.github.com>
2026-03-30 00:03:53 -0300

    Use environment variable for API_URL
    Replaced hardcoded API_URL with environment variable.

--- src/services/apiConfig.ts
+++ src/services/apiConfig.ts
@@ -4,7 +4,7 @@
 
 import axios, { type AxiosError, type AxiosResponse, type InternalAxiosRequestConfig } from "axios";
 
-const API_URL = "https://capacitaenlinea.cl/celtasksapi/api";
+const API_URL = import.meta.env.VITE_APP_API_URL;
 
 export const API_ERROR_CODES = {
     AUTH_MISSING: "AUTH_MISSING",
